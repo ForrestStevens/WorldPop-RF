3a5ee297456b4ce7b9d4e5d5d15cde582d412943
Forrest R. Stevens <forrest.stevens@louisville.edu>
2015-05-27 12:57:12 -0400

    Updated plotting code for the validation output.

--- src/01.Opt.2 - Validation.r
+++ src/01.Opt.2 - Validation.r
@@ -218,23 +218,27 @@ mae
 
 png(file=paste(output_path, "predicted_vs_observed_v", rf_version, ".png", sep=""))
 plot(y=predicted, x=observed, col=rgb(0,0,0,0.2), xlim=c(min(c(observed, predicted)), max(c(observed, predicted))), ylim=c(min(c(observed, predicted)), max(c(observed, predicted))), xlab="Observed", ylab="Predicted", pch=16, cex=0.7, main=paste("Estimated Counts From ", country_name, " vs. Finer Scale Unit Counts", sep=""))
+lines(lowess(y=predicted, x=observed), col=rgb(1,0,0,0.5), lty=2)
 abline(a=0, b=1, lty=2, col="darkgrey")
 
 text(y=max(c(observed, predicted))/10*4, x=max(c(observed, predicted))/10*5.5, paste("RMSE: ", format(rmse, nsmall=2, digits=2), sep=""), pos=4)
-text(y=max(c(observed, predicted))/10*3, x=max(c(observed, predicted))/10*5.5, paste("RMSE-Area: ", format(rmse_area, nsmall=2, digits=2), sep=""), pos=4)
+text(y=max(c(observed, predicted))/10*3, x=max(c(observed, predicted))/10*5.5, paste("RMSE/Area: ", format(rmse_area, nsmall=2, digits=2), sep=""), pos=4)
 text(y=max(c(observed, predicted))/10*2, x=max(c(observed, predicted))/10*5.5, paste("%RMSE: ", format(pct_rmse, nsmall=2, digits=2), sep=""), pos=4)
 text(y=max(c(observed, predicted))/10*1, x=max(c(observed, predicted))/10*5.5, paste("MAE: ", format(mae, nsmall=2, digits=2), sep=""), pos=4)
+legend("topright", legend=c("1:1", "lowess Fit"), lty=c(2,2), col=c("darkgrey", rgb(1,0,0,0.5)))
 dev.off()
 
 
 ##	Repeat the plot for visualizing in R:
 plot(y=predicted, x=observed, col=rgb(0,0,0,0.2), xlim=c(min(c(observed, predicted)), max(c(observed, predicted))), ylim=c(min(c(observed, predicted)), max(c(observed, predicted))), xlab="Observed", ylab="Predicted", pch=16, cex=0.7, main=paste("Estimated Counts From ", country_name, " vs. Finer Scale Unit Counts", sep=""))
+lines(lowess(y=predicted, x=observed), col=rgb(1,0,0,0.5), lty=2)
 abline(a=0, b=1, lty=2, col="darkgrey")
 
 text(y=max(c(observed, predicted))/10*4, x=max(c(observed, predicted))/10*5.5, paste("RMSE: ", format(rmse, nsmall=2, digits=2), sep=""), pos=4)
-text(y=max(c(observed, predicted))/10*3, x=max(c(observed, predicted))/10*5.5, paste("RMSE-Area: ", format(rmse_area, nsmall=2, digits=2), sep=""), pos=4)
+text(y=max(c(observed, predicted))/10*3, x=max(c(observed, predicted))/10*5.5, paste("RMSE/Area: ", format(rmse_area, nsmall=2, digits=2), sep=""), pos=4)
 text(y=max(c(observed, predicted))/10*2, x=max(c(observed, predicted))/10*5.5, paste("%RMSE: ", format(pct_rmse, nsmall=2, digits=2), sep=""), pos=4)
 text(y=max(c(observed, predicted))/10*1, x=max(c(observed, predicted))/10*5.5, paste("MAE: ", format(mae, nsmall=2, digits=2), sep=""), pos=4)
+legend("topright", legend=c("1:1", "lowess Fit"), lty=c(2,2), col=c("darkgrey", rgb(1,0,0,0.5)))
 
 
 ##	Return our working directory to the source folder:
